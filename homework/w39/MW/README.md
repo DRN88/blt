62290b720f1d6e5dc2e3f9df7f6100e6be713f0e
Watts <mike.watts@bauerservices.co.uk>
2016-10-20 12:52:55 +0100

    myhomework

--- homework/w39/MW/README.md
+++ homework/w39/MW/README.md
@@ -1,3 +1,1 @@
-## My Solution
-
-test pull request
+Readme
---
+++ homework/w39/MW/task3.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+### Tasks
+
+### 1. Download evilcorp_users1.csv using wget or curl.
+# Only keep one instance of evilcorp_users1.csv if I rerun task2.sh
+# https://raw.githubusercontent.com/DRN88/blt/master/w37/evilcorp_users1.csv
+# <<<write your code here >>>
+
+### 2. Count the cities. Print the results in the following format:
+### <city> found <count> times. Sort by the count Ascending. Display only the
+### first 20 results.
+egrep ",[[:alnum:]]*" evilcorp_users1.csv
+
+
+### 3. A company called 'Gosport' has been sold to 'Gigazoom'. Please update
+### their company name
+# <<<write your code here >>>
+
+
+### 4. All 'Sales Associate'-s been sacked. Please delete them.
+# <<<write your code here >>>
+
+
+### 5. Female 'Research Nurse'-ses got a promotion. They are now 'Senior Research Nurse'
+# <<<write your code here >>>
+
+
+### 6. Women lie about their age. Set all females birth date to 01/01/1988 who
+### is older than 1988.
+# <<<write your code here >>>
+
+
+### 7. Remove all 'Graphic Designer'-s ip address
+# <<<write your code here >>>
+
+
+### 8. There has been an error with our data. Rename all 'David'-s to 'Danielle'
+### who are female
+# <<<write your code here >>>
+
+
+### 9. Restructure our CSV to the following:
+### firstname,lastname,city,gender
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
